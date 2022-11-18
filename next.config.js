a673323c33c7e3507acf87aaee9f51c1b806a261
tahsinchouhan <tahsinchouhan2000@gmail.com>
2022-11-18 10:08:54 +0530

    added mongodb

--- next.config.js
+++ next.config.js
@@ -6,18 +6,20 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   images: {
-    domains: ["cdn.pixabay.com",
+    domains: [
+      "cdn.pixabay.com",
       "res.cloudinary.com",
       "images.unsplash.com",
       "thumbs.dreamstime.com",
-      "www.industrialempathy.com"
-   ]
+      "www.industrialempathy.com",
+    ],
   },
- 
+
   env: {
-    DATABASE_URL: 'mongodb+srv://partechidhievd:zPtYttu11IYJbNTc@paridhi.au9r2sm.mongodb.net/?retryWrites=true&w=majority',
-    JWT_SECRET: 'DrduNe1fh6goHN/L1^$%*&@^kLIytOIpSQS4a1',
-    PATH: 'http://localhost:3000',
+    DATABASE_URL:
+      "mongodb+srv://eknumbersauda:eknumber12345@cluster0.zibra8x.mongodb.net/eknumbersauda",
+    JWT_SECRET: "DrduNe1fh6goHN/L1^$%*&@^kLIytOIpSQS4a1",
+    PATH: "http://localhost:3000",
   },
   async headers() {
     return [
@@ -27,12 +29,17 @@ module.exports = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-
