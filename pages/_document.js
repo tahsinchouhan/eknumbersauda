9fe0004a41da578514f7dd29301ed1f74a76fc23
Tahsin <tahsinchouhan2000@gmail.com>
2023-06-16 16:19:13 +0530

    google tags added

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,22 +9,25 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="/dist/output.css" rel="stylesheet" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-4CQ2634ECB"
-        ></script>
-        <script
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-4CQ2634ECB');
-`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MNJ3XTH');`,
           }}
-        ></script>
+        ></Script>
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MNJ3XTH" height="0" width="0" style="display:none; visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>
